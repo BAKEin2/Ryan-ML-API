2ea7908f805b101db90ab73a70aa3c6475111b21
Ryan Tan <193190N@mymail.nyp.edu.sg>
2021-07-07 20:38:42 +0800

    1st commit

---
+++ visited-recommender.py
@@ -0,0 +1,29 @@
+from flask import Flask, app
+from flask import jsonify
+from flask import request
+
+import tensorflow as tf
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+model = tf.keras.models.load_model("irisclassifier.h5")
+
+@app.route("/classifyIris",methods=["POST"])
+def classifyIris():
+    input=request.get_json()
+
+    target=["iris_setosa","Iris-versicolor","Iris-virginica"]
+
+    sepalLength= input["sepalLength"]
+    sepalWidth= input["sepalWidth"]
+    petalLength= input["petalLength"]
+    petalWidth= input["petalWidth"]
+
+    result = model.predict([[sepalLength,sepalWidth,petalLength,petalWidth]])
+
+    return jsonify({ "result" : target[np.argmax(result[0])] })
+
+if __name__=="__main__":
+    app.run(debug=False)
