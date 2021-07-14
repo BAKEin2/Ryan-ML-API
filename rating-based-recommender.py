c8e6ee6f720f25727e408c81fc8c1bf13902bc5b
Ryan Tan <193190N@mymail.nyp.edu.sg>
2021-07-14 21:41:54 +0800

    changed a few things

--- rating-based-recommender.py
+++ rating-based-recommender.py
@@ -2,8 +2,9 @@ from flask import Flask, json
 from flask import jsonify
 from flask import request
 import pandas as pd
+import numpy as np
 import pickle
-
+from scipy.sparse.linalg import svds
 from joblib import dump,load
 #from flask_cors import CORS
 
@@ -11,37 +12,64 @@ app = Flask(__name__)
 #CORS(app)
 
 
-model = load("LoadedModels/rating_based_2.joblib")
-predicted_ratings_svd = pickle.load(open("LoadedModels/predicted_ratings_svd.dat", "rb"))
+#model = load("LoadedModels/rating_based_2.joblib")
+#predicted_ratings_svd = pickle.load(open("LoadedModels/predicted_ratings_svd.dat", "rb"))
+bakery_df = pd.read_csv('dataset/bakeries_location.csv')
 #pivot_data = pickle.load(open("pivot_data.dat", "rb"))
+
+def recommend_places(userID, pivot_data, pred_data, num_recommendations):
+    user_index  = userID-1 #index starts at 0
+
+    sorted_user_ratings = pivot_data.iloc[user_index].sort_values(ascending = False) #sort user ratings
+
+    sorted_user_predictions = pred_data.iloc[user_index].sort_values(ascending = False)#sorted_user_predictions
+    
+
+    temp = pd.concat([sorted_user_ratings, sorted_user_predictions], axis = 1)
+    temp.index.name = 'Recommended Places'
+    temp['Bakery_Name']=bakery_df['name']
+    temp['Types_bread']=bakery_df['types_of_bread']
+    temp.columns = ['user_ratings', 'user_predictions','Bakery_Name','Types_bread']
+    
+    temp = temp.loc[temp.user_ratings == 0]
+    temp = temp.sort_values('user_predictions', ascending = False)
+
+    print('\n Below are the recommended places for user(user_id = {}):\n'. format(userID))
+    return temp.head(num_recommendations)
 
 @app.route("/recommendBakeries",methods=["POST"])
 #@cross_origin()
-def recommend_places():
+def recommendBakeries():
     input=request.get_json()
-    bakery_df = pd.read_csv('dataset/bakeries_location.csv').to_json()
+    rating_df = pd.read_csv('dataset/userprofile_ratings.csv')
     #target=["name","placeID","types_of_bread",'user_ratings']
-    '''
+
     userid_input= input["userID"]
     rating_input = input["rating"]
     placeID_input = input["placeID"]
-    '''
-    userid_input= input["userID"]
-    user_ratings = predicted_ratings_svd[userid_input]
-    bakery_df['rating'] = user_ratings
-    bakery_df = bakery_df.sort_values(by=['rating'], ascending=False)
-    bakery_df = pd.DataFrame(bakery_df[['name', 'types_of_bread', 'rating']].head())
-    result=bakery_df.to_dict()
 
-    return jsonify({
-        "result":[
-            {"name" : result[0][0][1], "types_of_bread" : result[0][0][2],"rating": result[0][0][3]},
-            {"name" : result[0][1][1], "types_of_bread" : result[0][1][2],"rating": result[0][1][3]},
-            {"name" : result[0][2][1], "types_of_bread" : result[0][2][2],"rating": result[0][2][3]},
-            {"name" : result[0][3][1], "types_of_bread" : result[0][3][2],"rating": result[0][3][3]},
-            {"name" : result[0][4][1], "types_of_bread" : result[0][4][2],"rating": result[0][4][3]},
-        ]
-    })
+    new_rating = [[userid_input, placeID_input, rating_input]]
+    new_ratingDF = pd.DataFrame(new_rating, columns=['userID', 'placeID', 'rating'])
+    new_ratingDF
+
+    rating_df1=pd.concat([rating_df, new_ratingDF], ignore_index=True)
+
+    pivot_data = rating_df1.pivot_table(index = 'userID', columns = 'placeID', values = 'rating').fillna(0)
+
+    pivot_data['user_index'] = np.arange(0, pivot_data.shape[0],1)
+    pivot_data.set_index(['user_index'], inplace = True)
+
+    U,s, VT = svds(pivot_data, k = 10)
+    sigma = np.diag(s)
+    all_user_predicted_ratings = np.dot(np.dot(U,sigma), VT)
+
+    pred_data = pd.DataFrame(all_user_predicted_ratings, columns = pivot_data.columns)
+
+    userid_rating_input = input["userID_rating"]
+    result = recommend_places(userid_rating_input, pivot_data, pred_data, 5)
+
+    result1 = result.to_dict('records')
+    return jsonify({ "result":result1})
 
 if __name__=="__main__":
     app.run(debug=True)
