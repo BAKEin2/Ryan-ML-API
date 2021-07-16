4476f4670b7635330158fc4a809fde492474a967
Ryan Tan <58964130+RyanTan182@users.noreply.github.com>
2021-07-16 14:48:05 +0800

    added some vuejs stuff,but no work

--- myflaskapp/demo2app.js
+++ myflaskapp/demo2app.js
@@ -2,33 +2,92 @@ const demo2App = {
     
     data() {
         return {
-            sepalLength: "",
-            sepalWidth: "",
-            petalLength: "",
-            petalWidth: ""
+            longitude: "",
+            latitude: "",
+            bread_interests: "",
+            userID: "",
+            rating : "",
+            placeID : "",
+            userID_rating: "",
         }
     }, 
     methods: {
-        predictIrisSpecies: function() {
+        predictPopularBakeries: function() {
             console.log(JSON.stringify(
                 {
-                    sepalLength: this.sepalLength,
-                    sepalWidth: this.sepalWidth,
-                    petalLength: this.petalLength,
-                    petalWidth: this.petalWidth,
+                    longitude: this.longitude,
+                    latitude: this.latitude,
                 }
             ))
-            fetch('http://127.0.0.1:8000/classifyIris', {
+            fetch('http://127.0.0.1:5000/recommendPopularBakeries', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },                
                 body: JSON.stringify(
                     {
-                        sepalLength: parseFloat(this.sepalLength),
-                        sepalWidth: parseFloat(this.sepalWidth),
-                        petalLength: parseFloat(this.petalLength),
-                        petalWidth: parseFloat(this.petalWidth),
+                        longitude: parseFloat(this.longitude),
+                        latitude: parseFloat(this.latitude),
+                    }
+                )
+            })
+            .then(response => response.json())
+            .then(result => {
+                alert(result.result)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });            
+        },
+        predictInterestBakeries: function() {
+            console.log(JSON.stringify(
+                {
+                    longitude: this.longitude,
+                    latitude: this.latitude,
+                    bread_interests : this.bread_interests
+                }
+            ))
+            fetch('http://127.0.0.1:5000/recommendInterestBakeries', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },                
+                body: JSON.stringify(
+                    {
+                        longitude: parseFloat(this.longitude),
+                        latitude: parseFloat(this.latitude),
+                        bread_interests: String(this.bread_interests),
+                    }
+                )
+            })
+            .then(response => response.json())
+            .then(result => {
+                alert(result.result)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });            
+        },
+        predictRatedBakeries: function() {
+            console.log(JSON.stringify(
+                {
+                    userID: this.userID,
+                    rating: this.rating,
+                    placeID: this.placeID,
+                    userID_rating: this.userID_rating,
+                }
+            ))
+            fetch('http://127.0.0.1:5000/recommendRatedBakeries', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },                
+                body: JSON.stringify(
+                    {
+                        userID: parseInt(this.userID),
+                        rating: parseInt(this.rating),
+                        placeID: parseInt(this.placeID),
+                        userID_rating: parseInt(this.userID_rating),
                     }
                 )
             })
